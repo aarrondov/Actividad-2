c6c5d488d3e695343af776c6e76dba120162be9b
aarrondov <aarrondov@gmail.com>
2022-05-16 19:18:01 +0200

    Atributos arriba jugador, exitos sin tilde, correcion nombre getter y setter y switch con lambda

--- PiedraPapelTijeras/src/ende/Jugador.java
+++ PiedraPapelTijeras/src/ende/Jugador.java
@@ -1,34 +1,31 @@
 package ende;
 
 public class Jugador{
+    int exitos;      // número de partidas ganadas
+    int winTotal;
+
     /**
      * Escoge piedra, papel o tijera al azar
      */
     public String opcion_al_azar()
     {
         String opcion="";
-        Integer c = (int)(Math.random()*3);
-        switch(c){
-            case 0:
-                opcion=("piedra");
-                break;
-            case 1:
-                opcion=("papel");
-                break;
-            case 2:
-                opcion=("tijeras");
+        int c = (int)(Math.random()*3);
+        switch (c) {
+            case 0 -> opcion = ("piedra");
+            case 1 -> opcion = ("papel");
+            case 2 -> opcion = ("tijeras");
         }
         return opcion;
     }
-    public void setÉxitos()
+    public void setExitos()
     {
-        éxitos++;
+        exitos++;
     }
-    public int getÉxitos()
+    public int getExitos()
     {
-        return(éxitos);
+        return(exitos);
     }
 
-    int éxitos;      // número de partidas ganadas
-    int winTotal;
+
 }
